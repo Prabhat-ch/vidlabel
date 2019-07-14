949f2bc2b49fe511ec13e18aa7587341ecf92c26
Prabhat-ch <chprabhat2@gmail.com>
2019-07-14 07:43:05 +0545

    publish rekognition label to sns topic

--- vidlabel/handler.py
+++ vidlabel/handler.py
@@ -1,3 +1,5 @@
+import os
+
 import boto3
 import urllib.parse
 
@@ -10,7 +12,11 @@ def start_label_detection(bucket, keyname):
             'S3Object': {
                 'Bucket': bucket,
                 'Name': keyname
-            }
+            },
+        NotificationChannel={
+            'SNSTopicArn': os.environ('REKOGNITION_SNS_TOPIC_ARN'),
+            'RoleArn': os.environ('REKOGNITION_ROLE_ARN')
+        }
     })
 
     print(response)
@@ -25,5 +31,12 @@ def process_video(event, context):
             record['s3']['bucket']['name'],
             urllib.parse.unquote_plus(record['s3']['object']['key'])
         )
+
+    return
+
+
+def handle_label(event, context):
+
+    print(event)
 
     return
