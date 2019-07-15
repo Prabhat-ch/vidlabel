d586d24adb9ccc5bc8ac9c4de14e3a5c3f8636b3
Prabhat-ch <chprabhat2@gmail.com>
2019-07-15 10:41:38 +0545

    put labels in dynamodb

--- vidlabel/handler.py
+++ vidlabel/handler.py
@@ -21,8 +21,6 @@ def start_label_detection(bucket, keyname):
             'RoleArn': os.environ['REKOGNITION_ROLE_ARN']
     })
 
-    print(response)
-
     return
 
 def get_video_labels(job_id):
@@ -45,8 +43,36 @@ def get_video_labels(job_id):
     return response
 
 
+def make_item(data):
+    if isinstance(data, dict):
+        return { k: make_item(v) for k, v in data.items() }
+
+    if isinstance(data, list):
+        return [ make_item(v) for v in data ]
+
+    if isinstance(data, float):
+        return str(data)
+
+    return data
+
+
 def put_labels_in_db(data, s3_object, s3_bucket):
-    pass
+    del data['JobStatus']
+    del data['ResponseMetadata']
+
+    data['videoName'] = s3_object
+    data['videoBucket'] = s3_bucket
+
+    dynamodb = boto3.resource('dynamodb')
+    table_name = os.environ['DYNAMODB_TABLE_NAME']
+    videos_table = dynamodb.Table(table_name)
+
+    data = make_item(data)
+
+    videos_table.put_item(Item=data)
+
+    return
+
 
 # Lambda events
 
@@ -70,7 +96,6 @@ def handle_label(event, context):
         s3_bucket = message['Video']['S3Bucket']
 
         response = get_video_labels(job_id)
-        print(response)
         put_labels_in_db(response, s3_object, s3_bucket)
 
     return
